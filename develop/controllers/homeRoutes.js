7c0eebe4ec89ffa0019d4d104759fdb3efb66a4a
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-10-23 14:16:21 -0700

    get server data on homepage

--- develop/controllers/homeRoutes.js
+++ develop/controllers/homeRoutes.js
@@ -3,10 +3,10 @@ const { Blog, Workout, User } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-
+        const workoutData = await Workout.findAll()
         const blogData = await Blog.findAll({
             include: [
-                {
+                {   model: Workout,
                     model: User,
                     attributes: ['name'],
                     include: {
@@ -18,12 +18,13 @@ router.get('/', async (req, res) => {
         });
         // Serialize data so the template can read it
         const blogs = blogData.map((blog) => blog.get({ plain: true }));
-
+        const workouts = workoutData.map((workout) => workout.get({ plain: true}))
         res.render('homepage',{ 
+            workouts,
             blogs, 
             logged_in: req.session.logged_in 
           });
-        res.json(blogData);
+        // res.json(workouts)
     } catch (err) {
       res.status(500).json(err);
     }
