8f3053f5c2ba0413b8c888d59a26309a47f8983e
Chaggar25 <142560645+Chaggar25@users.noreply.github.com>
2023-10-25 13:15:40 -0400

    saving before merging for the day

--- develop/public/js/profile.js
+++ develop/public/js/profile.js
@@ -49,5 +49,33 @@
     });
   });
 
+  // Code Below is for the auto complete workout names filter
   
+  var workoutNames = [
+    "Squats",
+    "Push-ups",
+    "Deadlifts",
+    // Add more workout names
+  ];
+  
+
+  $(function () {
+    var workoutNames = [
+      "Squats",
+      "Push-ups",
+      "Deadlifts",
+      // Add more workout names
+    ];
+  
+    $("#tags").autocomplete({
+      source: function (request, response) {
+        var term = request.term.toLowerCase();
+        var filteredWorkouts = workoutNames.filter(function (workout) {
+          return workout.toLowerCase().includes(term);
+        });
+        response(filteredWorkouts);
+      },
+      minLength: 1,
+    });
+  });
   
