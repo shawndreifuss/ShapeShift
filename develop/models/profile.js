5fb1408031e5e41e2f429c8221eb36c6004b74e8
Chaggar25 <142560645+Chaggar25@users.noreply.github.com>
2023-10-24 23:29:09 -0400

    created a tab feature which seperates the body measurement and the initial profile page. also touched up the CSS alittle bit

--- develop/models/profile.js
+++ develop/models/profile.js
@@ -6,28 +6,7 @@ class profile extends Model {}
   
   
   
-  // code below is for the updated time zones// 
-    function populateTimezones() {
-      const timezonesSelect = document.getElementById("timezones");
-      const timezones = Intl.supportedValuesOf("timeZone");
-
-      for (const timezone of timezones ) {
-        const option = document.createElement("option");
-        option.value = timezone;
-        option.textContent = timezone;
-        timezonesSelect.appendChild(option);
-      }
-    }
-
-    window.onload = function() {
-      populateTimezones();
-    };
-
-    document.getElementById("timezones").addEventListener("change", function() {
-      const selectedTimezone = this.value;
-      // Perform actions based on the selected timezone, e.g., update the page with timezones as the user changes their time zone.// 
-      console.log("Selected Timezone: " + selectedTimezone);
-    });
+ 
     
   
     
